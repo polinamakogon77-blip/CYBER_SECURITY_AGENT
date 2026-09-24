ac73bf9408fb996bd54ae3fe4ff6200476958045
Makogon Polina <polinamakogon77@gmail.com>
2026-09-24 16:01:11 +0700

    добавлен Semgrep

--- tools/__init__.py
+++ tools/__init__.py
@@ -1,8 +1,11 @@
 # экспорт всех тулов агента
 from .check_idor import check_idor
+from .run_semgrep import run_semgrep
+
 
 ALL_TOOLS = [
-    check_idor
+    check_idor,
+    run_semgrep
 ]
 
-__all__ = ["ALL_TOOLS", "CHECK_IDOR"]
+__all__ = ["ALL_TOOLS", "CHECK_IDOR", "RUN_SEMGREP"]
